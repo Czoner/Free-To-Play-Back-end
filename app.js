e3d0f24ddc5a29974df479a0441c58f7388a5ce9
Cristofer Ledesma <ledes7080@gmail.com>
2024-11-04 15:17:15 -0500

    VM working on it

--- app.js
+++ app.js
@@ -20,12 +20,17 @@ app.use(express.json());
 
 app.use(cors());
 app.use(requestLogger);
+app.get("/crash-test", () => {
+    setTimeout(() => {
+        throw new Error("Server will crash now");
+    }, 0);
+});
 
 const proxyMiddleware = createProxyMiddleware({
     target: "https://www.freetogame.com/api",
     changeOrigin: true,
 });
-app.use("/api", proxyMiddleware);
+app.use("/", proxyMiddleware);
 app.use("/", mainRouter);
 app.use(errorLogger);
 app.use(errors());
